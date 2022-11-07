a25b4c1be588b503c47d466bc88ffe8280c0ed62
LellowMellow <79556112+LellowMellow@users.noreply.github.com>
2022-11-07 17:00:09 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M, depth;
+vector<bool> used;
+vector<int> result;
+
+void DFS(int count) {
+	if (count == M) {
+		for (auto iter : result) {
+			cout << iter << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			if (!used[i]) {
+				result.push_back(i + 1);
+				used[i] = true;
+				DFS(count + 1);
+				result.pop_back();
+				used[i] = false;
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N >> M;
+	
+	for (int i = 1; i <= N; i++) {
+		used.push_back(false);
+	}
+
+	DFS(0);
+
+	return 0;
+}
