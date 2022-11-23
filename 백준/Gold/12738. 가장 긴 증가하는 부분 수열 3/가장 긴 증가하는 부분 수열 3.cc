3f27c434f4377db80f7885a8b85d58b7797e8f70
LellowMellow <79556112+LellowMellow@users.noreply.github.com>
2022-11-24 02:12:56 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 192 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, x;
+vector<int> LIS;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		auto iter = lower_bound(LIS.begin(), LIS.end(), x);
+		if (iter == LIS.end()) {
+			LIS.push_back(x);
+		}
+		else {
+			*iter = x;
+		}
+	}
+	cout << LIS.size() << "\n";
+
+	return 0;
+}
