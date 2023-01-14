0c2c958203e25417c9010b784f4915b8af3e31af
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-01-14 16:41:01 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,3 +1,3 @@
 function solution(array) {
-    return array.sort(function(a, b) { return a - b; })[Math.floor(array.length / 2)];
+    return array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
 }
