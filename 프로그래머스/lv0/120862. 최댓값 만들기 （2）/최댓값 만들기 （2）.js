bd3ba66a31c5b180aa8867e463989bc99465ac97
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-03-16 16:58:22 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,6 +1,4 @@
 function solution(numbers) {
-    let minus = numbers.filter((n) => n < 0).sort((a, b) => a - b);
     numbers.sort((a, b) => b - a);
-    if(minus.length < 2) return numbers[0] * numbers[1];
-    else return Math.max(numbers[0] * numbers[1], minus[0] * minus[1]);
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
 }
