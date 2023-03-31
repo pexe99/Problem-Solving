3a32f350d60ad5fb6be9e9fea34aa6877392f493
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-03-31 13:37:03 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,6 +1,6 @@
 function solution(n) {
     let current = 1, factorial = 1;
-    while(current * factorial < n) {
+    while(current * (factorial + 1) <= n) {
         current *= ++factorial;
     }
     return factorial;
