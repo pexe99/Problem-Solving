ccbafeb4e7c6f930a1503233dca5ec20f2be118e
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-07 15:05:25 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,5 +1,5 @@
 function solution(id_pw, db) {
-    if(db.filter((cur) => cur[0] === id_pw[0] && cur[1] === id_pw[1]).length) return "login";
-    else if(db.filter((cur) => cur[0] === id_pw[0]).length) return "wrong pw";
-    else return "fail";
+    const [id, pw] = id_pw;
+    let dbMap = new Map(db);
+    return dbMap.has(id) ? (dbMap.get(id) === pw ? "login" : "wrong pw") : "fail";
 }
