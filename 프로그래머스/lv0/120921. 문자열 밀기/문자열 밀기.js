276ef308164c78229b39a9012723c55f1d787a40
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-08 23:07:21 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,7 +1,1 @@
-function solution(A, B) {
-    for(let i = 0; i <= A.length; i++) {
-        if(A === B) return i;
-        A = A[A.length - 1] + A.slice(0, A.length - 1);
-    }
-    return -1;
-}
+const solution = (A, B) => (B + B).indexOf(A);
