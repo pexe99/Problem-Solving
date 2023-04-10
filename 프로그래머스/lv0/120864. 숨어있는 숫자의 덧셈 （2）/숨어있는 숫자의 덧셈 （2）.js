dd392daeca8fc3e47c912dc6767ffd7929e00ba1
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-10 18:06:56 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    let array = my_string.replace(/[a-z]+/gi, " ").split(" ").filter((cur) => cur !== " ");
-    if(array.length === 0) return 0;
-    else return Number(array.reduce((cur, sum) => sum = Number(sum) + Number(cur)));
+    return my_string.split(/\D+/).reduce((cur, sum) => Number(sum) + Number(cur), 0);
 }
