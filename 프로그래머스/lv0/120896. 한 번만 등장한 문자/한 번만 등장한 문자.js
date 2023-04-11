e936a441635d1c264f5a1596337d8db53513c237
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-11 19:09:12 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    let counter = {};
+    let result = [];
+    
+    [...s].forEach((cur) => {
+        if(cur in counter) counter[cur]++;
+        else counter[cur] = 1;
+    })
+    
+    for(cur in counter) {
+        if(counter[cur] === 1) result.push(cur);
+    }
+    
+    return result.sort().join("");
+}
