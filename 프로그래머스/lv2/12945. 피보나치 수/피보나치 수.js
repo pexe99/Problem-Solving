70897e33e16e27981cc0be61c3eb7d4f9730ad29
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-14 14:53:29 +0900

    [level 2] Title: 피보나치 수, Time: 1.60 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,11 @@
 function solution(n) {
-    let fibo = [0 ,1];
+    let fiboA = 0, fiboB = 1;
     
     for(let i = 2; i <= n; i++) {
-        fibo.push((fibo[i - 1] + fibo[i - 2]) % 1234567);    
+        let temp = fiboB;
+        fiboB = (fiboA + fiboB) % 1234567;
+        fiboA = temp;
     }
     
-    return fibo[n];
+    return fiboB;
 }
