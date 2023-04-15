d4e37b3075de076ebf56f8374862dd63df5fde87
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-15 22:32:07 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,7 +1,7 @@
 function solution(brown, yellow) {  
     for(let i = 1; i <= yellow; i++) {
         if(yellow % i === 0 && brown + yellow === (i + 2) * (yellow / i + 2)) {
-            return [Math.max(i, yellow / i) + 2, Math.min(i, yellow / i) + 2];
+            return [yellow / i + 2, i + 2];
         }
     }
 }
