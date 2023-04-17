32eefdbc5aefa55cfdec1a8e7a1ba20dc09f123c
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-17 16:50:54 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,14 +1,14 @@
 function solution(progresses, speeds) {
-    let result = [];
-    while(progresses.length) {
-        let counter = 0;
-        while(progresses[0] >= 100) {
-            progresses.shift();
-            speeds.shift();
-            counter++;
+    let result = [0];
+    let days = progresses.map((v, idx) => Math.ceil((100 - v) / speeds[idx]));
+    
+    for(let i = 0, j = 0, max = days[0]; i < days.length; i++) {
+        if(days[i] <= max) result[j]++;
+        else {
+            max = days[i];
+            result[++j] = 1;
         }
-        progresses = progresses.map((v, idx) => v + speeds[idx]);
-        if(counter !== 0) result.push(counter);
     }
+    
     return result;
 }
