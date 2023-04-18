473da348bf990e5f8d74311297fd5a40e74115fd
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-18 16:34:24 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,15 +1,7 @@
 function solution(arr) {
-    arr.sort((a, b) => b - a);
-    let result = LCM(arr[0], arr[1]);
-    
-    for(let i = 2; i < arr.length; i++) {
-        result = LCM(result, arr[i]);
-    }
-    
-    return result;
+    return arr.reduce((lcm, cur) => lcm * cur / gcd(lcm, cur));
 }
 
-function LCM(a, b) {
-    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
-    return a * b / gcd(a, b);
+function gcd(a, b) {
+    return a % b ? gcd(b, a % b) : b;
 }
