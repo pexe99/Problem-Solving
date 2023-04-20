53a9c112a88d19d90dba072e1f8386e0720b1a96
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-20 21:15:10 +0900

    [unrated] Title: 그림 확대, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
@@ -0,0 +1,10 @@
+function solution(picture, k) {
+    let result = [];
+    
+    picture.map((v) => {
+        const cur = [...v].map((c) => c.repeat(k)).join('');
+        for(let i = 0; i < k; i++) result.push(cur);
+    });
+    
+    return result;
+}
