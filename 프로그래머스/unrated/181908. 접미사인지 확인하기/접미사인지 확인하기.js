25c438b33b959e6db5d977fb32bd29296e840427
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-22 18:07:56 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,3 @@
 function solution(my_string, is_suffix) {
-    return [...my_string].reverse().join('').indexOf([...is_suffix].reverse().join('')) === 0 ? 1 : 0;
+    return Number(my_string.slice(my_string.length - is_suffix.length) === is_suffix);
 }
