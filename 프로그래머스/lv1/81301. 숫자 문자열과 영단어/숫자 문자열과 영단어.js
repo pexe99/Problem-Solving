3db0f27adec8a722e4aa177d03b427a758c76a83
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-22 18:28:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,7 +1,9 @@
 function solution(s) {
     const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     
-    numbers.forEach((v, idx) => s = s.replaceAll(v, String(idx)));
+    numbers.forEach((v, idx) => {
+        s = s.split(v).join(idx);
+    });
     
     return Number(s);
 }
