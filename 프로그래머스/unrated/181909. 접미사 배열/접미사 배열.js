08261aa637233f60a0a73c877aa4085e642e3b9a
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-27 16:56:32 +0900

    [unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -1,7 +1,7 @@
 function solution(my_string) {
     let result = [];
-    for(let i = 1; i <= my_string.length; i++) {
-        result.push(my_string.slice(my_string.length - i, my_string.length));
+    for(let i = 0; i < my_string.length; i++) {
+        result.push(my_string.slice(i));
     }
     return result.sort();
 }
