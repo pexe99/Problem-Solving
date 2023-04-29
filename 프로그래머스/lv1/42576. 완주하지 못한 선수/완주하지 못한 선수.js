c6e253e6a3bb3547bfb251707cdb3f857aa3d7d0
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-04-29 18:31:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.20 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,12 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    
-    for(let i = 0; i < participant.length; i++){
-        if(participant[i] !== completion[i]) return participant[i];
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        map.set(participant[i], (map.get(participant[i]) || 0) + 1);
+        map.set(completion[i], (map.get(completion[i]) || 0) - 1);
+    }
+
+    for(let [key, value] of map) {
+        if(value > 0) return key;
     }
 }
