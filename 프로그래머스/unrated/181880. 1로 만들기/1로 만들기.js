6bc958ccc567547cba4c3a38e47b82a310365b87
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-05-04 20:51:11 +0900

    [unrated] Title: 1로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,11 +1,3 @@
 function solution(num_list) {
-    return num_list.reduce((r, v) => {
-        let cur = 0;
-        while(v !== 1) {
-            v = Math.floor(v / 2);
-            cur++;
-        }
-        r += cur;
-        return r;
-    }, 0);
+    return num_list.reduce((r, v) => r + v.toString(2).length - 1, 0);
 }
