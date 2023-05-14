91f0b457c997af0fac99eedfcbe6c578b1997dee
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-05-14 16:03:37 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,8 +1,6 @@
 function solution(n) {
     for(let i = 1; i <= n; i++) {
-        if([...String(i)].includes("3") || i % 3 === 0) {
-            n++;
-        }
+        if([...String(i)].includes("3") || i % 3 === 0) n++;
     }
     return n;
 }
