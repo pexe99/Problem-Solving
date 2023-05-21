ef9d01cc1dbdf9a58953361eb9bd753f9e07710a
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-05-21 15:25:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -3,19 +3,13 @@ function solution(s) {
     
     for(let i = 0; i < s.length; i++) {
         s = s.slice(1) + s[0];
-        let stack = [];
-        let current = true;
-        for(let j = 0; j < s.length; j++) {
-            if(s[j] === '(' || s[j] === '{' || s[j] === '[') stack.push(s[j]);
-            else if(s[j] === ')' && stack.at(-1) === '(') stack.pop();
-            else if(s[j] === '}' && stack.at(-1) === '{') stack.pop();
-            else if(s[j] === ']' && stack.at(-1) === '[') stack.pop();
-            else {
-                current = false;
-                break;
-            }
+        let current = s;
+        while(true) {
+            const curLength = current.length;
+            current = current.replace(/\(\)|\[\]|\{\}/g, '');
+            if(curLength === current.length) break;
         }
-            if(stack.length === 0 && current) result++;
+        if(current === '') result++;
     }
     
     return result;
