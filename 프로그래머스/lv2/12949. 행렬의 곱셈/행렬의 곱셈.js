ab59c1d61ed775dbf280213cc7d1a7e4988cf623
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-05-22 19:41:01 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.79 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,10 +1,7 @@
 function solution(arr1, arr2) {
-    let result = Array.from({length: arr1.length}, () => []);
-    for(let i = 0; i < arr1.length; i++) {
-        for(let j = 0; j < arr2[0].length; j++) {
-            let current = arr1[i].reduce((s, v, idx) => s + (v * arr2[idx][j]), 0);
-            result[i].push(current);
-        }
-    }
-    return result;
+    return arr1.map((row) => {
+        return arr2[0].map((_, i) => {
+            return row.reduce((s, v, idx) => s + v * arr2[idx][i], 0);
+        })
+    })
 }
