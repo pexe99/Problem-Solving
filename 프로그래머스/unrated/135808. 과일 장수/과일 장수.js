9f3daf4ee2e3f9f2f7dac01b469d5166f5bf450f
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-05-23 14:35:27 +0900

    [unrated] Title: 과일 장수, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,24 +1,5 @@
 function solution(k, m, score) {
-    let countApple = new Array(k).fill(0);
-    score.forEach((a) => countApple[a - 1]++);
-    
-    let curBox = 0, result = 0, idx = k - 1;
-    while(idx >= 0) {
-        if(curBox + countApple[idx] <= m) {
-            curBox += countApple[idx];
-            countApple[idx] = 0;
-            if(curBox === m) {
-                result += m * (idx + 1);
-                curBox = 0;
-            }
-            idx--;
-        }
-        else {
-            countApple[idx] -= m - curBox;
-            result += m * (idx + 1);
-            curBox = 0;
-        }
-    }
-    
-    return result;
+    score.sort((a, b) => b - a);
+    let minApple = score.filter((_, i) => (i + 1) % m === 0);
+    return minApple.reduce((s, v) => s + v, 0) * m;
 }
