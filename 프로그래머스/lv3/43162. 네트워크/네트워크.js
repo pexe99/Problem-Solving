70ebc2999339b534d1eb6b6001374f3c69f1b854
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-05-30 21:03:55 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,20 @@
+function solution(n, computers) {
+    let visited = new Array(n).fill(false);
+    let result = 0;
+    
+    function DFS(current) {
+        visited[current] = true;  
+        computers[current].forEach((v, i) => {
+            if(v === 1 && !visited[i]) DFS(i);
+        })
+    }
+    
+    for(let i = 0; i < n; i++) {
+        if(!visited[i]) {
+            result++;
+            DFS(i);
+        }
+    }
+    
+    return result;
+}
