e1a78a63b40a7df0c8825b93c2c00d4d3944f44e
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-06-03 19:06:04 +0900

    [level 2] Title: 튜플, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,11 +1,6 @@
 function solution(s) {
-    let answer = [];
-    const tuple = s.slice(2, s.length - 2).split("},{").map((v) => v.split(','));
+    let tuple = JSON.parse(s.replace(/\{/g, '[').replace(/\}/g, ']'));
     tuple.sort((a, b) => a.length - b.length);
-    tuple.forEach((array) => {
-        array.forEach((v) => {
-            if(!answer.includes(Number(v))) answer.push(Number(v));
-        })
-    });
-    return answer;
+    const array = tuple.reduce((s, v) => s.concat(v), []);
+    return [...new Set(array)];
 }
