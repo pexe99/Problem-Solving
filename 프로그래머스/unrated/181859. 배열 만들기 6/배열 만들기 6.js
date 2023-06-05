e1db509c3264d49b488d2ce8b8c88c01b44ad3a9
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-06-05 20:12:58 +0900

    [unrated] Title: 배열 만들기 6, Time: 9.30 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,14 +1,9 @@
 function solution(arr) {
     let stk = [], i = 0;
     while(arr.length > i) {
-        if(stk.at(-1) === arr[i]) {
-            stk.pop();
-            i++;
-        }
-        else {
-            stk.push(arr[i]);
-            i++;
-        }
+        if(stk.at(-1) === arr[i]) stk.pop();
+        else stk.push(arr[i]);
+        i++;
     }
     return stk.length ? stk : [-1];
 }
