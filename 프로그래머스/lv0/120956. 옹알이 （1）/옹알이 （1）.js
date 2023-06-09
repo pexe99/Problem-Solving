2845f8d48e7fb74d9e606f3194a7ddc67eb0ca01
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-06-09 19:38:46 +0900

    [level 0] Title: 옹알이 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,10 +1,8 @@
 function solution(babbling) {
-    const babble = ["aya", "ye", "woo", "ma"];
-    return babbling.reduce((result, current) => {
-        babble.forEach((v) => {
-            current = current.split(v).join(',');
-        });
-        console.log(current);
-        return [...current].filter((v) => v !== ',').length ? result : result + 1;
-    }, 0);
+    let result = 0;
+    const regex = /^(aya|ye|woo|ma)+$/;
+    babbling.forEach((v) => {
+        if(regex.test(v)) result++;
+    });
+    return result;
 }
