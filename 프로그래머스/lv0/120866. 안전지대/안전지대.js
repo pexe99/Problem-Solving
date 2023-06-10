62e45f4cdf25a09b6d90075fd4e662ed27e6e451
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-06-10 17:57:50 +0900

    [level 0] Title: 안전지대, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,14 +1,9 @@
 function solution(board) {
-    board.forEach((b, idx1) => {
-        b.forEach((v, idx2) => {
-            if(v === 1) {
-                for(let i = idx1 - 1; i <= idx1 + 1; i++) {
-                    for(let j = idx2 - 1; j <= idx2 + 1; j++) {
-                        if(i >= 0 && j >= 0 && i < board.length && j < board.length && board[i][j] !== 1) board[i][j] = 2;
-                    }
-                }
-            }
-        }) 
-    })
-    return board.reduce((s, b) => s + b.filter((v) => v === 0).length, 0);
+    let safeCounter = 0;
+    const around = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+    board.forEach((b, idx1) => b.forEach((v, idx2) => {
+        if(v === 1) return false;
+        return around.some(([x, y]) => !!board[x + idx1] && board[x + idx1][y + idx2] === 1) ? false : safeCounter++;
+    }));
+    return safeCounter;
 }
