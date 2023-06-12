8c60e279e78e89945b9dde845f10502b8980f87a
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-06-12 17:03:47 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.59 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,35 +1,19 @@
 function solution(today, terms, privacies) {
     const type = {};
-    let result = [];
+    const [curYear, curMonth, curDate] = today.split('.').map(Number);
+    const current = curYear * 12 * 28 + curMonth * 28 + curDate;
     terms.forEach((v) => {
         v = v.split(' ');
         type[v[0]] = +v[1];
     });
     
+    let result = [];
     privacies.forEach((v, i) => {
         v = v.split(' ');
-        if(checkTerm(type[v[1]], today, v[0])) result.push(i + 1);
+        let [year, month, date] = v[0].split('.').map(Number);
+        let duration = year * 12 * 28 + month * 28 + date + type[v[1]] * 28;
+        if(duration <= current) result.push(i + 1);
     });
     
     return result;
-}
-
-function checkTerm(duration, current, when) {
-    let [curYear, curMonth, curDate] = current.split('.').map((v) => +v);
-    let [year, month, date] = when.split('.').map((v) => +v);
-    
-    curDate -= date
-    if(curDate < 0) {
-        curMonth--;
-        curDate += 28;
-    }
-    curMonth -= month
-    if(curMonth <= 0) {
-        curYear--;
-        curMonth += 12;
-    }
-    curYear -= year;
-    
-    if(curYear * 12 + curMonth >= duration) return true;
-    return false;
 }
