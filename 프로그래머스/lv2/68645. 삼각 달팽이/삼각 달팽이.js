a5e661805f3c4bc19031da9397130ddef547ebe5
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-07-04 00:54:01 +0900

    [level 2] Title: 삼각 달팽이, Time: 106.48 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,37 +1,15 @@
 function solution(n) {
     let triangle = Array.from({length : n}, (_, i) => new Array(i + 1).fill(0));
     
-    let idx1 = 0, idx2 = 0, direction = 1, total = 0;
-    triangle.forEach((v) => total += v.length);
+    let idx1 = -1, idx2 = 0, number = 0;
     
-    for(let i = 1; i <= total; i++) {
-        triangle[idx1][idx2] = i;
-        if(direction === 1) {
-            if(!triangle.at(idx1 + 1) || triangle[idx1 + 1][idx2] !== 0) {
-                idx2++;
-                direction = 2;
-            }
-            else idx1++;
-        }
-        else if(direction === 2) {
-            if(!!triangle[idx1][idx2 + 1] || triangle[idx1][idx2 + 1] !== 0) {
-                idx1--;
-                idx2--;
-                direction = 3;
-            }
-            else idx2++;
-        }
-        else {
-            if(triangle[idx1 - 1][idx2 - 1] !== 0) {
-                idx1++;
-                direction = 1;
-            }
-            else {
-                idx1--;
-                idx2--;
-            }
-        }
+    for(let i = n; i > 0; i -= 3) {
+        triangle[++idx1][idx2] = ++number;
+        
+        for(let j = 0; j < i - 1; j++) triangle[++idx1][idx2] = ++number;
+        for(let j = 0; j < i - 1; j++) triangle[idx1][++idx2] = ++number;
+        for(let j = 0; j < i - 2; j++) triangle[--idx1][--idx2] = ++number;
     }
     
-    return triangle.reduce((result, v) => result.concat(v), []);
+    return triangle.flat();
 }
