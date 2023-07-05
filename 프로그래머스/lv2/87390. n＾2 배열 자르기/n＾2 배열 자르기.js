9f39341393822b899e2946cbd91cb5552c8c7e30
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2023-07-05 21:27:49 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.12 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,10 +1,8 @@
 function solution(n, left, right) {
-    let minimum = Math.ceil((left + 1) / n), array = [];
+    let array = [];
     
-    for(let i = left + 1; i <= right + 1; i++) {
-        let current = i % n === 0 ? n : i % n;
-        array.push(minimum > current ? minimum : current);
-        if(i % n === 0) minimum++;
+    for(let i = left; i <= right; i++) {
+        array.push(Math.max(i % n, Math.floor(i / n)) + 1);
     }
     
     return array;
