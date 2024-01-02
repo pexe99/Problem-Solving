a856501c9885b861db4250f1172ecf717ee464fb
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-01-02 16:58:25 +0900

    [level 2] Title: 전화번호 목록, Time: 99.40 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,6 +1,4 @@
 function solution(phone_book) {
-    let result = true;
     let array = phone_book.sort();
-    result = !array.some((value, index, array) => array[index + 1]?.substr(0, value.length) === value);
-    return result;
+    return !array.some((value, index, array) => array[index + 1]?.substr(0, value.length) === value);
 }
