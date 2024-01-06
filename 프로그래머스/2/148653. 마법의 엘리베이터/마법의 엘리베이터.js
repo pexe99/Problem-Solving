87fd01131dd494bd5e530123f0b9528d5ab46b85
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-01-07 08:27:32 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -4,14 +4,7 @@ function solution(storey) {
         let current = storey % 10;
         storey = Math.floor(storey / 10);
         let next = storey % 10;
-        if(current < 5) result += current;
-        else if(current === 5) {
-            if(next < 5) result += current;
-            else {
-                result += (10 - current);
-                storey++;
-            }
-        }
+        if(current < 5 || (current === 5 && next < 5)) result += current;
         else {
             result += (10 - current);
             storey++;
