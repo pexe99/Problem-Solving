13b9a4b591052b5cb1675628ff39e08a9c69800c
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-01-31 11:40:31 +0900

    [level 2] Title: 리코쳇 로봇, Time: 2.68 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -0,0 +1,49 @@
+const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+];
+
+const solution = (board) => {
+    const boardX = board.length;
+    const boardY = board[0].length;
+    
+    let robot = [0, 0];
+    board = board.map((string, index) => {
+        if(string.indexOf("R") !== -1) robot = [index, string.indexOf("R")];
+        return string.split("");
+    });
+    
+    const BFS = (start) => {
+        let queue = [], result = -1;
+        queue.push({curX: robot[0], curY: robot[1], curDist: 0});
+        
+        while(queue.length) {
+            let {curX, curY, curDist} = queue.shift();
+            if(board[curX][curY] === 'G') {
+                result = curDist;
+                break;
+            }
+            board[curX][curY] = 'V';
+            directions.forEach(([x, y]) => {
+                let nextX = curX, nextY = curY;
+                while(0 <= nextX && nextX < boardX && 0 <= nextY && nextY < boardY && board[nextX]?.[nextY] !== 'D') {
+                    nextX += x;
+                    nextY += y;
+                }
+                nextX -= x;
+                nextY -= y;
+                if(board[nextX] && (board[nextX][nextY] === '.' || board[nextX][nextY] === 'G')) 
+                   queue.push({curX: nextX, curY: nextY, curDist: curDist + 1});
+            })
+        }
+        return result;
+    }
+    
+    return BFS(robot);
+}
+
+const consoleBoard = (board) => {
+    board.forEach((array) => console.log(array.join("")));
+}
