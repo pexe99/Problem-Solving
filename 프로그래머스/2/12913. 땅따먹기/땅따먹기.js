ed39ceadd145aad71dfad1f683af776182f423f1
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-02-08 22:14:34 +0900

    [level 2] Title: 땅따먹기, Time: 72.11 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,10 @@
+function solution(land) {
+    for(let i = 1; i < land.length; i++) {
+        for(let j = 0; j < 4; j++) {
+            land[i][j] += Math.max(
+               ...land[i - 1].filter((_, index) => index !== j));   
+        }
+    }
+    
+    return Math.max(...land.at(-1))
+}
