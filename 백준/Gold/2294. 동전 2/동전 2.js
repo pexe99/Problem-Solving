b139e585b1f8097481ba21205de08a05399d2370
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-02-17 18:17:25 +0900

    [Gold V] Title: 동전 2, Time: 192 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.js
+++ 백준/Gold/2294. 동전 2/동전 2.js
@@ -9,17 +9,15 @@ let coins = input
   .map((e) => +e)
   .sort((a, b) => a - b);
 
-let dp = Array.from({ length: N + 1 }, (_) =>
-  Array.from({ length: K + 1 }, (_, i) => (i === 0 ? 0 : Infinity))
-);
+let dp = Array.from({ length: K + 1 }, (_, i) => (i === 0 ? 0 : Infinity));
 
 for (let i = 1; i <= N; i++) {
   let coin = coins[i - 1];
   for (let j = 1; j <= K; j++) {
     if (coin <= j) {
-      dp[i][j] = Math.min(1 + dp[i][j - coin], dp[i - 1][j]);
-    } else dp[i][j] = dp[i - 1][j];
+      dp[j] = Math.min(1 + dp[j - coin], dp[j]);
+    }
   }
 }
 
-console.log(dp[N][K] === Infinity ? -1 : dp[N][K]);
+console.log(dp[K] === Infinity ? -1 : dp[K]);
