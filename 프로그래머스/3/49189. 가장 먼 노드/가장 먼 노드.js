6ca1122a79816a522c9d6129a4c22f878819f94b
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-02-28 19:28:45 +0900

    [level 3] Title: 가장 먼 노드, Time: 24.49 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -5,15 +5,19 @@ function solution(n, edge) {
         graph[b - 1].push(a - 1);
     });
     
-    let distance = Array.from({length: n}, () => Infinity);
-    let queue = [[0, 0]];
+    let visited = Array.from({length: n}, () => false);
+    let queue = [0], counter = n;
     while(queue.length) {
-        let [current, n] = queue.shift();
-        if(distance[current] === Infinity) {
-            distance[current] = n;
-            graph[current].forEach((e) => queue.push([e, n + 1]));
-        }
+        let current = [...queue], next = new Set();
+        current.forEach((node) => visited[node] = true);
+        current.forEach((node) => {
+            graph[node].forEach((e) => {
+                if(!visited[e]) next.add(e);
+            });
+            counter--;
+        });
+        queue = [...next];
+        if(counter === queue.length) break;
     }
-    let max = Math.max(...distance);
-    return distance.filter((e) => e === max).length;
+    return counter;
 }
