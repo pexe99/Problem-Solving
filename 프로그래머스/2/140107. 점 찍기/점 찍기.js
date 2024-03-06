e9ac77f92dee453b3fa3423de3c5eae50738dc7b
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-03-06 23:09:46 +0900

    [level 2] Title: 점 찍기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -0,0 +1,7 @@
+function solution(k, d) {
+    let result = 1 + Math.floor(d / k) * 2;
+    for(let a = 1; a < d / k + 2; a++) {
+        result += Math.floor(Math.sqrt(Math.pow(d / k, 2) - Math.pow(a, 2))) || 0;
+    }
+    return result;
+}
