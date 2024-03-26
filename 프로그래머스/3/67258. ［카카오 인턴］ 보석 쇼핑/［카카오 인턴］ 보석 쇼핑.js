143565c401e2b1ec975e9a7f20499e6ad765f436
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-03-26 11:32:27 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 19.25 ms, Memory: 40.2 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,11 +1,13 @@
 function solution(gems) {
     let [start, end] = [0, -1], [resStart, resEnd] = [-1, gems.length];
-    let current = new Map(), isAll = new Set(), kind = new Set(gems);
-    kind.forEach((e) => current[e] = 0);
-    while(end < gems.length - 1) {
-        current[gems[++end]]++; isAll.add(gems[end]);
-        while(current[gems[start]] > 1) current[gems[start++]]--;
-        if(isAll.size === kind.size && resEnd - resStart > end - start) {
+    let current = new Map(), types = new Set(gems).size;
+    while(++end < gems.length) {
+        current.set(gems[end], (current.get(gems[end]) || 0) + 1);
+        while(current.get(gems[start]) > 1) {
+            current.set(gems[start], current.get(gems[start]) - 1);
+            start++;
+        }
+        if(current.size === types && resEnd - resStart > end - start) {
             [resStart, resEnd] = [start, end];
         }
     }
