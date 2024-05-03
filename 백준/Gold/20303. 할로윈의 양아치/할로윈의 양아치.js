44f44ad6ebd125237950584b524c2d3e63fb16c9
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-05-03 15:24:45 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 612 ms, Memory: 53488 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.js
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.js
@@ -1,58 +1,39 @@
-const fs = require("fs");
-const stdin = fs
-  .readFileSync("/dev/stdin")
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
   .toString()
-  .trim()
-  .split("\n")
-  .map((ele) => ele.split(" ").map((ele) => +ele));
+  .split("\n");
 
-const [N, M, K] = stdin[0];
-const candyNumArr = [...stdin[1]];
-const edgeInfo = stdin.slice(2);
-
-const unionList = new Array(N + 1).fill(0);
-for (let i = 0; i <= N; i++) {
-  unionList[i] = i;
-}
+const [[N, M, K], candies, ...edges] = input.map((str) =>
+  str.split(" ").map((e) => +e)
+);
+const knapsack = {};
+const dp = Array.from({ length: K + 1 }, () => 0);
+const unionArr = Array.from({ length: N + 1 }, (_, i) => i);
 
 const findRoot = (node) => {
-  if (unionList[node] === node) return node;
-
-  return (unionList[node] = findRoot(unionList[node]));
+  return unionArr[node] === node
+    ? node
+    : (unionArr[node] = findRoot(unionArr[node]));
 };
-const union = (a, b) => {
-  let parentA = findRoot(a);
-  let parentB = findRoot(b);
 
-  if (parentA < parentB) {
-    unionList[parentB] = parentA;
-  } else {
-    unionList[parentA] = parentB;
+const union = (node1, node2) => {
+  let [rootA, rootB] = [findRoot(node1), findRoot(node2)];
+  if (rootA < rootB) unionArr[rootA] = rootB;
+  else unionArr[rootB] = rootA;
+};
+
+edges.forEach(([a, b]) => union(a, b));
+for (let i = 1; i <= N; i++) {
+  let root = findRoot(i);
+  if (!knapsack[root]) knapsack[root] = { child: 0, candy: 0 };
+  knapsack[root].child++;
+  knapsack[root].candy += candies[i - 1];
+}
+
+Object.values(knapsack).forEach(({ candy: v, child: w }) => {
+  for (let i = K; i >= 1; i--) {
+    if (w < i) dp[i] = Math.max(dp[i], v + dp[i - w]);
   }
-};
-
-edgeInfo.forEach(([f1, f2]) => {
-  union(f1, f2);
 });
 
-const friendNumCostArr = Array.from({ length: N + 1 }, () => [0, 0]);
-
-for (let i = 1; i < N + 1; i++) {
-  const root = findRoot(i);
-  friendNumCostArr[root][0] += 1;
-  friendNumCostArr[root][1] += candyNumArr[i - 1];
-}
-
-const dp = Array.from({ length: K }, () => 0);
-
-for (let i = 1; i <= N; i++) {
-  if (findRoot(i) !== i) continue;
-  for (let j = K - 1; j >= 0; j--) {
-    const [friendNum, cost] = friendNumCostArr[i];
-    if (j - friendNum >= 0) {
-      dp[j] = Math.max(dp[j], dp[j - friendNum] + cost);
-    }
-  }
-}
-
-console.log(Math.max(...dp));
+console.log(dp[K]);
