7d9bbc2623c88624a1988aabfc07315ba3034397
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-10-20 22:29:39 +0900

    [level 3] Title: 110 옮기기, Time: 224.54 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
@@ -1,28 +1,24 @@
 function solution(s) {
     return s.map((current) => {
-        let result = "";
-        while (true) {
-            let counter = 0;
-            let array = [...current].reduce((acc, cur) => {
-                if(cur === '0' && acc.length >= 2 && acc.at(-1) + acc.at(-2) === '11') {
-                    acc.pop();
-                    acc.pop();
-                    counter++;
-                } else acc.push(cur);
-                return acc;
-            }, [])
-            let next = array.join("");
-            let insertIndex = 0;
-            for (let i = next.length - 1; i >= 0; i--) {
-                if (next[i] === '0') {
-                    insertIndex = i + 1;
-                    break;
-                }
+        current = [...current]; 
+        let counter = 0;
+        let next = current.reduce((acc, cur) => {
+            if(cur === '0' && acc.length >= 2 && acc.at(-1) + acc.at(-2) === '11') {
+                acc.pop();
+                acc.pop();
+                counter++;
+            } else acc.push(cur);
+            return acc;
+        }, [])
+        let insertIndex = 0;
+        for (let i = next.length - 1; i >= 0; i--) {
+            if (next[i] === '0') {
+                insertIndex = i + 1;
+                break;
             }
-            next = next.substring(0, insertIndex) + "110".repeat(counter) + next.substring(insertIndex);
-            if (current === next) break;
-            current = next;
         }
-        return current;
+        let poppedString = "110".repeat(counter);
+        next = [...next.slice(0, insertIndex), ...poppedString, ...next.slice(insertIndex)];
+        return next.join("");
     });
 }
--- 프로그래머스/3/77886. 110 옮기기/README.md
+++ 프로그래머스/3/77886. 110 옮기기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 110 옮기기 - 77886 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77886#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77886) 
 
 ### 성능 요약
 
-메모리: 82.4 MB, 시간: 280.12 ms
+메모리: 109 MB, 시간: 224.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 20일 22:21:34
+2024년 10월 20일 22:29:36
 
 ### 문제 설명
 
