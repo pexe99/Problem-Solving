5665c7b24644d8a513c24f43cd9f6bd18596489f
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-10-21 20:29:57 +0900

    [level 3] Title: 스타 수열, Time: 355.19 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.js
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.js
@@ -1,20 +1,21 @@
 function solution(a) {
-    const starSequence = {};
-    new Set(a).forEach((key) => starSequence[key] = {index: -1, stack: []});
+    if(a.length < 2) return 0;
+    let starSequence = {};
+    new Set(a).forEach((key) => starSequence[key] = {index: -1, len: 0});
     for(let i = 1; i < a.length; i++) {
         let [first, second] = [a[i - 1], a[i]];
         if(first !== second) {
             if(starSequence[first].index < i - 1) {
                 starSequence[first].index = i;
-                starSequence[first].stack.push([first, second]);
+                starSequence[first].len++;
             }
             if(starSequence[second].index < i - 1) {
                 starSequence[second].index = i;
-                starSequence[second].stack.push([first, second]);
+                starSequence[second].len++;
             }
         }
     }
-    let star = Object.keys(starSequence)
-        .sort((a, b) => starSequence[b].stack.length - starSequence[a].stack.length)[0];
-    return starSequence[star].stack.length * 2;
+    let result = 0;
+    Object.values(starSequence).forEach((cur) => result = Math.max(result, cur.len));
+    return result * 2;
 }
