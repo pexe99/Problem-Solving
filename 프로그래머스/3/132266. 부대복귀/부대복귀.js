6291b4a6c8cec25b6a673e9c3c2a261a91dbe858
이동령 <79556112+LellowMellow@users.noreply.github.com>
2024-10-24 18:02:46 +0900

    [level 3] Title: 부대복귀, Time: 965.88 ms, Memory: 212 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,3 +1,27 @@
+class Heap {
+    constructor() {
+        this.outstack = [];
+        this.instack = [];
+    }
+    
+    enqueue(value) {
+        this.instack.push(value);
+    }
+    
+    dequeue() {
+        if(this.outstack.length === 0) {
+            while(this.instack.length) {
+                this.outstack.push(this.instack.pop());
+            }
+        }
+        return this.outstack.pop();
+    }
+    
+    length() {
+        return this.instack.length + this.outstack.length;
+    }
+}
+
 function solution(n, roads, sources, destination) {
     const graph = {};
     roads.forEach(([a, b]) => {
@@ -6,22 +30,23 @@ function solution(n, roads, sources, destination) {
     });
     const distance = Array.from({length: n + 1}, (_, index) => Infinity);
     
-    const dijkstra = (start) => {
-        const queue = [start];
+    const BFS = (start) => {
+        const queue = new Heap();
+        queue.enqueue(start);
         distance[start] = 0;
-        while(queue.length) {
-            let current = queue.shift();
+        while(queue.length()) {
+            let current = queue.dequeue();
             graph[current].forEach((next) => {
                 if(distance[next] !== Infinity) {
                     distance[next] = Math.min(distance[next], distance[current] + 1);
                 } else {
                     distance[next] = distance[current] + 1;
-                    queue.push(next);
+                    queue.enqueue(next);
                 }
             })
         }
     }
     
-    dijkstra(destination);
+    BFS(destination);
     return sources.map((current) => distance[current] === Infinity ? -1 : distance[current]);
 }
