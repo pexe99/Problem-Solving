3cd72bb192a749caa32ec6f39f0786b099553da3
이동령 <79556112+LellowMellow@users.noreply.github.com>
2025-01-19 16:07:47 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 40.54 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,24 +1,10 @@
-const getLeastNum = (number) => {
-    let binaryNum = [...number.toString(2)];
-    if(binaryNum.filter((e) => e === "0").length === 0)
-        return parseInt(["1", "0", ...binaryNum.slice(1)].join(""), 2);
-    for(let i = binaryNum.length - 1; 0 <= i; i--) {
-        if(binaryNum[i] === "0") {
-            binaryNum[i] = "1";
-            if(i !== binaryNum.length - 1) {
-                for(let j = i + 1; j < binaryNum.length; j++) {
-                    if(binaryNum[j] === "1") {
-                        binaryNum[j] = "0";
-                        break;
-                    }
-                }
-            }
-            break;
-        }
-    }
-    return parseInt(binaryNum.join(""), 2);
-}
-
 function solution(numbers) {
-    return numbers.map((number) => getLeastNum(number));
+    return numbers.map((number) => {
+        if(number % 2 === 0) return number + 1;
+        let iterator = 1;
+        while(iterator *= 2) {
+            if((number + 1) % (iterator * 2) !== 0) break;
+        }
+        return number + (iterator / 2);
+    });
 }
