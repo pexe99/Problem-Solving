c21d5a40e33945428294de50326fddbbdde36cee
이동령 <79556112+LellowMellow@users.noreply.github.com>
2025-02-07 01:31:20 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.70 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,17 +1,13 @@
-const SCORE = [null, 3, 2, 1, 0, 1, 2, 3];
-
 function solution(survey, choices) {
-    const testScore = {R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0};
+    const score = {};
+    const types = ["RT", "CF", "JM", "AN"];
+    
+    types.join("").split("").forEach((type) => score[type] = 0);
+    
     choices.forEach((choice, index) => {
         const [left, right] = survey[index].split("");
-        if(choice < 4) testScore[left] += SCORE[choice];
-        else if(4 < choice) testScore[right] += SCORE[choice];
+        score[choice <= 4 ? left : right] += Math.abs(4 - choice);
     });
-    let testResult = "";
-    const testScoreKey = Object.keys(testScore);
-    while(testScoreKey.length) {
-        const [type1, type2] = testScoreKey.splice(0, 2);
-        testResult += testScore[type1] < testScore[type2] ? type2 : type1;
-    }
-    return testResult;
+    
+    return types.map(([t1, t2]) => score[t1] < score[t2] ? t2 : t1).join("");
 }
