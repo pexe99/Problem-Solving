8ce7ba8679826c3b63fbe0077989ab4bafd17ff9
이동령 <79556112+pexe99@users.noreply.github.com>
2025-10-15 12:18:46 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 38.68 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,21 @@
+function solution(bridge_length, weight, truck_weights) {
+    let timer = 0;
+    let bridgeLoad = 0;
+    const onBridge = [];
+    
+    while(truck_weights.length || onBridge.length) {
+        timer++;
+        if(onBridge[0]?.[1] + 1 === bridge_length) {
+                bridgeLoad -= onBridge[0][0];
+                onBridge.shift();
+        }
+        onBridge.forEach(([truck, counter], index) => {
+            onBridge[index][1]++;
+        });
+        if(bridgeLoad + truck_weights[0] <= weight && onBridge.length < bridge_length) {
+            bridgeLoad += truck_weights[0];
+            onBridge.push([truck_weights.shift(), 0]);
+        }
+    }
+    return timer;
+}
