29f2254c98838cf0634ab070e563947e5148ed6f
이동령 <79556112+pexe99@users.noreply.github.com>
2025-11-06 21:09:00 +0900

    [level 2] Title: 하노이의 탑, Time: 15.88 ms, Memory: 49.7 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/README.md
+++ 프로그래머스/2/12946. 하노이의 탑/README.md
@@ -1,10 +1,10 @@
 # [level 2] 하노이의 탑 - 12946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946?language=javascript) 
 
 ### 성능 요약
 
-메모리: 49.5 MB, 시간: 16.54 ms
+메모리: 49.7 MB, 시간: 15.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 06일 21:05:22
+2025년 11월 06일 21:08:57
 
 ### 문제 설명
 
--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,8 +1,10 @@
 function solution(n) {
+    const hanoi = (from, to, through, n) => {
+        if(n === 1) return [[from, to]];
+        return [...hanoi(from, through, to, n - 1), 
+                [from, to],
+               ...hanoi(through, to, from, n - 1)];
+    }
+    
     return hanoi(1, 3, 2, n);
-}
-
-const hanoi = (from, to, through, n) => {
-    if(n === 1) return [[from, to]];
-    return hanoi(from, through, to, n - 1).concat([[from, to]]).concat(hanoi(through, to, from, n - 1));
 }
