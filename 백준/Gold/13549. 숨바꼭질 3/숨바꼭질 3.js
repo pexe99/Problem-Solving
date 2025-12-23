699283475da1da38437164e2dd98efa887ef702c
이동령 <79556112+pexe99@users.noreply.github.com>
2025-12-23 14:54:24 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 188 ms, Memory: 19424 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -1,4 +1,5 @@
-const input = require("fs")
+const fs = require("fs");
+const input = fs
   .readFileSync(
     process.platform === "linux" ? "/dev/stdin" : "input.txt",
     "utf8"
@@ -7,34 +8,32 @@ const input = require("fs")
   .split("\n");
 
 const MAXIMUM = 100000;
-const [N, K] = input[0].split(" ").map(Number);
 
-const deque = new Map();
-const dist = new Array(MAXIMUM + 1).fill(Infinity);
+const [[N, K]] = input.map((string) => string.split(" ").map(Number));
 
-dist[N] = 0;
-deque.set(0, N);
-let front = 0,
-  back = 1;
+const solution = (N, K) => {
+  const queue = new Array(MAXIMUM * 2);
+  const dist = new Array(MAXIMUM + 1).fill(Infinity);
+  let [head, tail] = [MAXIMUM, MAXIMUM + 1];
 
-while (front < back) {
-  const current = deque.get(front);
-  deque.delete(front++);
+  dist[N] = 0;
+  queue[head] = N;
+  while (head < tail) {
+    const current = queue[head++];
+    if (current === K) return dist[K];
 
-  if (current === K) {
-    console.log(dist[K]);
-    break;
-  }
-
-  for (const [next, cost] of [
-    [current * 2, 0],
-    [current + 1, 1],
-    [current - 1, 1],
-  ]) {
-    if (next < 0 || MAXIMUM < next) continue;
-    if (dist[next] > dist[current] + cost) {
-      dist[next] = dist[current] + cost;
-      cost === 0 ? deque.set(--front, next) : deque.set(back++, next);
+    for (const [next, cost] of [
+      [current * 2, 0],
+      [current - 1, 1],
+      [current + 1, 1],
+    ]) {
+      if (next < 0 || MAXIMUM < next) continue;
+      if (dist[next] > dist[current] + cost) {
+        dist[next] = dist[current] + cost;
+        cost === 0 ? (queue[--head] = next) : (queue[tail++] = next);
+      }
     }
   }
-}
+};
+
+console.log(solution(N, K));
