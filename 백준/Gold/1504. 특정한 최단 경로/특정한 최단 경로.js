fe21774ae52c40abb3e0ce18997da05416d9a0f6
이동령 <79556112+pexe99@users.noreply.github.com>
2026-01-16 19:03:20 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 644 ms, Memory: 94324 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.js
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.js
@@ -11,15 +11,72 @@ const [N, M] = input[0].split(" ").map(Number);
 const edges = input.slice(1, M + 1).map((e) => e.split(" ").map(Number));
 const [v1, v2] = input[input.length - 1].split(" ").map(Number);
 
+class Heap {
+  constructor(compare) {
+    this.heap = [null];
+    this.compare = compare;
+  }
+
+  _swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
+
+  push(value) {
+    this.heap.push(value);
+
+    let current = this.heap.length - 1;
+    let parent = current >> 1;
+    while (
+      parent !== 0 &&
+      this.compare(this.heap[current], this.heap[parent])
+    ) {
+      this._swap(current, parent);
+      current = parent;
+      parent = current >> 1;
+    }
+  }
+
+  pop() {
+    if (this.heap.length === 1) return null;
+    if (this.heap.length === 2) return this.heap.pop();
+
+    const root = this.heap[1];
+    this.heap[1] = this.heap.pop();
+    const size = this.heap.length;
+
+    let current = 1;
+    while (true) {
+      let [target, left, right] = [current, current * 2, current * 2 + 1];
+      if (left < size && this.compare(this.heap[left], this.heap[target]))
+        target = left;
+      if (right < size && this.compare(this.heap[right], this.heap[target]))
+        target = right;
+      if (target === current) break;
+      this._swap(current, target);
+      current = target;
+    }
+    return root;
+  }
+}
+
 const dijikstra = (start, graph) => {
   const distance = new Array(graph.length).fill(Infinity);
-  const queue = [start];
+  const pq = new Heap((a, b) => a[1] < b[1]);
   distance[start] = 0;
-  for (let i = 0; i < queue.length; i++) {
-    graph[queue[i]].forEach(([next, weight]) => {
-      if (distance[queue[i]] + weight < distance[next]) {
-        queue.push(next);
-        distance[next] = distance[queue[i]] + weight;
+  pq.push([start, 0]);
+
+  while (pq.size()) {
+    const [current, curDistance] = pq.pop();
+    if (curDistance > distance[current]) continue;
+    graph[current].forEach(([next, weight]) => {
+      const nextDistance = curDistance + weight;
+      if (nextDistance < distance[next]) {
+        pq.push([next, nextDistance]);
+        distance[next] = nextDistance;
       }
     });
   }
