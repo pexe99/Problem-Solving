30e0daa164da18d72fc47e41098bc53af538d40d
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-03 16:41:00 +0900

    [Gold I] Title: 계단 수, Time: 164 ms, Memory: 11964 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.js
+++ 백준/Gold/1562. 계단 수/계단 수.js
@@ -13,12 +13,12 @@ const FULL_MASK = (1 << 2) - 1;
 
 const getStairNumber = (digit, len, bitmask, N, stairNumber) => {
   if (stairNumber[digit][len][bitmask]) return stairNumber[digit][len][bitmask];
-  else if (len === N) return bitmask === FULL_MASK ? 1 : 0;
+  else if (len === 0) return bitmask === FULL_MASK ? 1 : 0;
   else {
     if (digit < 9)
       stairNumber[digit][len][bitmask] += getStairNumber(
         digit + 1,
-        len + 1,
+        len - 1,
         bitmask | (digit + 1 === 9 ? 2 : 0),
         N,
         stairNumber
@@ -26,7 +26,7 @@ const getStairNumber = (digit, len, bitmask, N, stairNumber) => {
     if (digit > 0)
       stairNumber[digit][len][bitmask] += getStairNumber(
         digit - 1,
-        len + 1,
+        len - 1,
         bitmask | (digit - 1 === 0 ? 1 : 0),
         N,
         stairNumber
@@ -44,7 +44,13 @@ const solution = (N) => {
   for (let i = 1; i <= 9; i++)
     result =
       (result +
-        getStairNumber(i, 1, i === 0 ? 1 : i === 9 ? 2 : 0, N, stairNumber)) %
+        getStairNumber(
+          i,
+          N - 1,
+          i === 0 ? 1 : i === 9 ? 2 : 0,
+          N,
+          stairNumber
+        )) %
       MOD;
   return result;
 };
