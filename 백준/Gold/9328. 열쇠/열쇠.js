a54581ce2d7a14e0743a6b1bc5dac86429a5b0b9
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-11 02:26:16 +0900

    [Gold I] Title: 열쇠, Time: 280 ms, Memory: 19268 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.js
+++ 백준/Gold/9328. 열쇠/열쇠.js
@@ -9,124 +9,87 @@ const input = fs
 
 const T = +input.shift();
 
-class Building {
-  WALL = "*";
-  EMPTY = ".";
-  DOCUMENT = "$";
-  REGEXP_KEY = /^[a-z]$/;
-  REGEXP_DOOR = /^[A-Z]$/;
-  DIRECTION = [
-    [0, 1],
-    [1, 0],
-    [0, -1],
-    [-1, 0],
-  ];
+const WALL = "*";
+const EMPTY = ".";
+const DOCUMENT = "$";
+const REGEXP_KEY = /^[a-z]$/;
+const REGEXP_DOOR = /^[A-Z]$/;
+const DIRECTION = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
 
-  constructor(h, w, map, keyString) {
-    this.h = h;
-    this.w = w;
-    this.map = map;
-    this.key = new Set();
-    this.visited = Array.from({ length: h }, () =>
-      Array.from({ length: w }, () => false)
-    );
+const isBorder = (i, j, h, w) =>
+  0 === i || i === h - 1 || 0 === j || j === w - 1;
 
-    keyString !== "0" && keyString.split("").forEach((e) => this.key.add(e));
-  }
+const isAvailable = (i, j, h, w, visited) =>
+  0 <= i && i < h && 0 <= j && j < w && visited !== WALL && !visited[i][j];
 
-  updateKey(i, j) {
-    this.key.add(this.map[i][j]);
-  }
+const getStealableDocumentNumber = (h, w, map, keyString) => {
+  const door = {};
+  const queue = [];
+  const key = new Set(keyString === "0" ? [] : [...keyString]);
+  const visited = Array.from({ length: h }, () =>
+    Array.from({ length: w }, () => false)
+  );
 
-  isWall(i, j) {
-    return this.WALL === this.map[i][j];
-  }
+  let counter = 0;
 
-  isEmpty(i, j) {
-    return this.EMPTY === this.map[i][j];
-  }
-
-  isDocument(i, j) {
-    return this.DOCUMENT === this.map[i][j];
-  }
-
-  isKey(i, j) {
-    return this.REGEXP_KEY.test(this.map[i][j]);
-  }
-
-  isDoor(i, j) {
-    return this.REGEXP_DOOR.test(this.map[i][j]);
-  }
-
-  isDoorAvailable(i, j) {
-    return this.key.has(this.map[i][j].toLowerCase());
-  }
-
-  isAvailable(i, j) {
-    return (
-      0 <= i &&
-      i < this.h &&
-      0 <= j &&
-      j < this.w &&
-      !this.visited[i][j] &&
-      !this.isWall(i, j)
-    );
-  }
-
-  isBorder(i, j) {
-    return i === 0 || i === this.h - 1 || j === 0 || j === this.w - 1;
-  }
-
-  getStealableDocs() {
-    let result = 0;
-    const door = {};
-    const queue = [];
-
-    for (let i = 0; i < this.h; i++) {
-      for (let j = 0; j < this.w; j++) {
-        if (!this.isBorder(i, j) || this.isWall(i, j)) continue;
-        else if (this.isDoor(i, j) && !this.isDoorAvailable(i, j)) {
-          door[this.map[i][j].toLowerCase()] = [
-            ...(door[this.map[i][j].toLowerCase()] || []),
-            [i, j],
-          ];
-          continue;
-        } else if (this.isKey(i, j)) this.updateKey(i, j);
-        else if (this.isDocument(i, j)) result++;
-        queue.push([i, j]);
-        this.visited[i][j] = true;
-      }
-    }
-
-    for (let index = 0; index < queue.length; index++) {
-      const [ci, cj] = queue[index];
-      for (const [di, dj] of this.DIRECTION) {
-        const [ni, nj] = [ci + di, cj + dj];
-        if (!this.isAvailable(ni, nj)) continue;
-        else if (this.isDoor(ni, nj) && !this.isDoorAvailable(ni, nj)) {
-          door[this.map[ni][nj].toLowerCase()] = [
-            ...(door[this.map[ni][nj].toLowerCase()] || []),
-            [ni, nj],
-          ];
-          continue;
-        } else if (this.isDocument(ni, nj)) result++;
-        else if (this.isKey(ni, nj)) {
-          this.updateKey(ni, nj);
-          if (door[this.map[ni][nj]]) {
-            door[this.map[ni][nj]].forEach(([dri, drj]) => {
-              this.visited[dri][drj] = true;
-              queue.push([dri, drj]);
-            });
-          }
+  map.forEach((row, i) =>
+    row.forEach((e, j) => {
+      if (isBorder(i, j, h, w)) {
+        if (e === EMPTY) {
+          queue.push([i, j]);
+        } else if (e === DOCUMENT) {
+          counter++;
+          queue.push([i, j]);
+        } else if (REGEXP_DOOR.test(e)) {
+          const doorKey = e.toLowerCase();
+          if (key.has(doorKey)) {
+            queue.push([i, j]);
+          } else door[doorKey] = [...(door[doorKey] || []), [i, j]];
+        } else if (REGEXP_KEY.test(e)) {
+          key.add(e);
+          queue.push([i, j]);
         }
-        queue.push([ni, nj]);
-        this.visited[ni][nj] = true;
+        visited[i][j] = true;
       }
-    }
+    })
+  );
 
-    return result;
+  key.forEach((e) => door[e] && queue.push(...door[e]));
+
+  for (let index = 0; index < queue.length; index++) {
+    const [ci, cj] = queue[index];
+    DIRECTION.forEach(([di, dj]) => {
+      const [ni, nj] = [ci + di, cj + dj];
+      if (isAvailable(ni, nj, h, w, visited)) {
+        const e = map[ni][nj];
+        if (e === EMPTY) {
+          queue.push([ni, nj]);
+        } else if (e === DOCUMENT) {
+          counter++;
+          queue.push([ni, nj]);
+        } else if (REGEXP_DOOR.test(e)) {
+          const doorKey = e.toLowerCase();
+          if (key.has(doorKey)) {
+            queue.push([ni, nj]);
+          } else door[doorKey] = [...(door[doorKey] || []), [ni, nj]];
+        } else if (REGEXP_KEY.test(e)) {
+          key.add(e);
+          queue.push([ni, nj]);
+          if (door[e]) queue.push(...door[e]);
+          door[e] = [];
+        }
+        visited[ni][nj] = true;
+      }
+    });
   }
-}
+
+  return counter;
+};
 
 const solution = (T, input) => {
   const result = [];
@@ -135,8 +98,7 @@ const solution = (T, input) => {
     const [h, w] = input.shift().split(" ").map(Number);
     const map = input.splice(0, h).map((e) => e.split(""));
     const keyString = input.shift();
-    const building = new Building(h, w, map, keyString);
-    result.push(building.getStealableDocs());
+    result.push(getStealableDocumentNumber(h, w, map, keyString));
   }
 
   return result.join("\n");
