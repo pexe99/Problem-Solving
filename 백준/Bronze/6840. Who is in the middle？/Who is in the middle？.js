fe74d24f9127bc0c2c55f6a44a8c984b90d9c9f0
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-18 16:35:03 +0900

    [Bronze V] Title: Who is in the middle?, Time: 84 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(input.map(Number).sort((a, b) => a - b)[1]);
