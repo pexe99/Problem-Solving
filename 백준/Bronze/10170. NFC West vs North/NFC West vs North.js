1740303d0f7c938ae36b93991798468f4f9e35f4
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-18 17:18:22 +0900

    [Bronze V] Title: NFC West vs North, Time: 80 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(`NFC West       W   L  T
+-----------------------
+Seattle        13  3  0
+San Francisco  12  4  0
+Arizona        10  6  0
+St. Louis      7   9  0
+
+NFC North      W   L  T
+-----------------------
+Green Bay      8   7  1
+Chicago        8   8  0
+Detroit        7   9  0
+Minnesota      5  10  1`);
