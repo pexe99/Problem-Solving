f0cfafb738110ed2fa70e89e69b68cba9f7f16f9
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-18 16:28:47 +0900

    [Bronze V] Title: 아!, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(input[0].length >= input[1].length ? "go" : "no");
