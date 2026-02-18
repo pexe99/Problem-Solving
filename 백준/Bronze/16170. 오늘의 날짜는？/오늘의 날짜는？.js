dfd34b27c34c5f31b2940d000b659afa859b564d
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-18 18:04:03 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 84 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+const today = new Date();
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, "0");
+const day = String(today.getDate()).padStart(2, "0");
+console.log(`${year}\n${month}\n${day}`);
