32ccfd79fed8513546b92056bb0a09071eee779c
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-19 16:13:39 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 80 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+const [a, b, c] = input.map(BigInt);
+console.log(String((b - c) / a));
