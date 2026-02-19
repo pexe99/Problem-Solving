e8cb4608cc35f99cee5394f5fa20de20b3dbb763
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-19 16:40:21 +0900

    [Bronze V] Title: Copier, Time: 80 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(
+  input
+    .slice(1)
+    .map((e) => `${e} ${e}`)
+    .join("\n")
+);
