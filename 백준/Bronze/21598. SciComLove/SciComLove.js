f72991fd290c2c09e06aa277840d1c3a322ab2dd
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-19 15:40:38 +0900

    [Bronze V] Title: SciComLove, Time: 92 ms, Memory: 9324 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(new Array(+input[0]).fill("SciComLove").join("\n"));
