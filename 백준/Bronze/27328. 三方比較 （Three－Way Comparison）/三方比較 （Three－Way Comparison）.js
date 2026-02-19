ee8ae58b846da9856929d720cfb14fbb0702feab
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-19 16:49:45 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 88 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(
+  input.reduce((acc, cur, idx) =>
+    idx ? (acc < cur ? -1 : acc === cur ? 0 : 1) : cur
+  )
+);
