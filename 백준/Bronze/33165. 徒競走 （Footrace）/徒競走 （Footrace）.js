d3ebf25892085f3be0ec46a1f9249846e754f990
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-24 00:03:42 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 88 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+const [T, V] = input.map(Number);
+console.log(T * V);
