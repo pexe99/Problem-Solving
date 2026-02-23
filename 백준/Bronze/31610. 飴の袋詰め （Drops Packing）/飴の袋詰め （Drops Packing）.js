7a1ece5eda24f7e452b9f7e3a0234c23823c7861
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-23 23:39:19 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+const [A, B, C] = input.map(Number);
+console.log(A * B + C);
