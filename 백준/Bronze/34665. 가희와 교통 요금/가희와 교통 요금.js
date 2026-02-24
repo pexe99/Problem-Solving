71321e3832485e060d39c0af4994f92e95ef68e5
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-24 16:56:20 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+const [A, B] = input;
+console.log(A === B ? 0 : 1550);
