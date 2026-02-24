a59c0dfd91e5529ae1133e53215568c11fe4d54f
이동령 <79556112+pexe99@users.noreply.github.com>
2026-02-24 16:48:23 +0900

    [Bronze V] Title: 채플이 너무해, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(6 <= +input[0] ? "Success!" : "Oh My God!");
