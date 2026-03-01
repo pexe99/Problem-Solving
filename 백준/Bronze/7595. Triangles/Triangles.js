c7de47f35801731f7591091ac7a47142f0e9b2a2
이동령 <79556112+pexe99@users.noreply.github.com>
2026-03-02 00:05:02 +0900

    [Bronze IV] Title: Triangles, Time: 112 ms, Memory: 11356 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+input
+  .slice(0, input.length - 1)
+  .forEach((e) =>
+    console.log(
+      Array.from({ length: e }, (_, i) => "*".repeat(i + 1)).join("\n")
+    )
+  );
