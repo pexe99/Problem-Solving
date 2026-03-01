bcc4cbb0c4f0edb2eda135d727d9649cdbe8af0f
이동령 <79556112+pexe99@users.noreply.github.com>
2026-03-01 17:22:25 +0900

    [Bronze IV] Title: Multiple Choice, Time: 92 ms, Memory: 9616 KB -BaekjoonHub

---
+++ 백준/Bronze/6784. Multiple Choice/Multiple Choice.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(
+  input.slice(1, +input[0] + 1).filter((e, i) => e === input[i + +input[0] + 1])
+    .length
+);
