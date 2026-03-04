55b749378e3a264adfb383c3970b66218fad52c9
이동령 <79556112+pexe99@users.noreply.github.com>
2026-03-04 18:12:16 +0900

    [Bronze IV] Title: Quick Estimates, Time: 100 ms, Memory: 9572 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+input.slice(1).forEach((e) => console.log(e.length));
