3ceb649c3a028324a93acfd1d3fe4fa6fcf0fad3
이동령 <79556112+pexe99@users.noreply.github.com>
2026-03-06 17:49:08 +0900

    [Bronze IV] Title: 3 つの整数 (Three Integers), Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/18408. 3 つの整数 （Three Integers）/3 つの整数 （Three Integers）.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(
+  input[0]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b)[1] === 1
+    ? 1
+    : 2
+);
