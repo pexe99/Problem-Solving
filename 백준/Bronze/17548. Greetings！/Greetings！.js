b27c4f85aa87f6c33adca01200e22c7b65b129e4
이동령 <79556112+pexe99@users.noreply.github.com>
2026-03-06 17:18:48 +0900

    [Bronze IV] Title: Greetings!, Time: 92 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/17548. Greetings！/Greetings！.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? 0 : "input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+console.log(
+  input[0]
+    .split("")
+    .map((e) => (e === "e" ? "ee" : e))
+    .join("")
+);
