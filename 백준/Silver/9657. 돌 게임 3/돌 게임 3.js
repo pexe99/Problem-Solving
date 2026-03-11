f941f7da15c2039bd5ac185508d4d25269274ac3
이동령 <79556112+pexe99@users.noreply.github.com>
2026-03-11 21:27:02 +0900

    [Silver III] Title: 돌 게임 3, Time: 152 ms, Memory: 9384 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.js
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.js
@@ -4,18 +4,4 @@ const input = fs
   .trim()
   .split("\n");
 
-const N = +input[0];
-
-const solution = (N) => {
-  const dp = new Array(N + 1).fill(false);
-  dp[1] = dp[3] = dp[4] = true;
-
-  for (let i = 5; i <= N; i++)
-    dp[i] = [dp[i - 1], dp[i - 3], dp[i - 4]].filter((e) => !e).length
-      ? true
-      : false;
-
-  return dp[N] ? "SK" : "CY";
-};
-
-console.log(solution(N));
+console.log(["CY", "SK", "CY", "SK", "SK", "SK", "SK"][+input[0] % 7]);
