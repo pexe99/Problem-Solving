590c7ba47f5d5f74c9da663fd6a64a33e37b44af
이동령 <79556112+pexe99@users.noreply.github.com>
2026-03-20 01:42:07 +0900

    [Gold III] Title: LCA, Time: 420 ms, Memory: 57376 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.js
+++ 백준/Gold/11437. LCA/LCA.js
@@ -9,40 +9,55 @@ const edges = input.splice(0, N - 1).map((line) => line.split(" ").map(Number));
 const M = +input.shift();
 const nodes = input.map((line) => line.split(" ").map(Number));
 
+const query = (LOG, parent, depth, a, b) => {
+  if (depth[b] < depth[a]) [a, b] = [b, a];
+
+  for (let k = LOG; k >= 0; k--)
+    if ((depth[b] - depth[a]) & (1 << k)) b = parent[k][b];
+
+  if (a === b) return a;
+
+  for (let k = LOG; k >= 0; k--) {
+    if (parent[k][a] !== parent[k][b]) {
+      a = parent[k][a];
+      b = parent[k][b];
+    }
+  }
+
+  return parent[0][a];
+};
+
 const solution = (N, edges, M, nodes) => {
-  const parent = new Array(N + 1).fill(0);
-  const depth = new Array(N + 1).fill(null);
-  const visited = new Array(N + 1).fill(false);
+  const depth = new Int32Array(N + 1).fill(-1);
   const graph = Array.from({ length: N + 1 }, () => []);
   edges.forEach(([a, b]) => {
     graph[a].push(b);
     graph[b].push(a);
   });
 
-  depth[1] = 0;
-  visited[1] = true;
+  const LOG = Math.ceil(Math.log2(N));
+  const parent = Array.from({ length: LOG + 1 }, () => new Int32Array(N + 1));
 
+  depth[1] = 0;
   let index = 0;
   const queue = [[1, 0]];
   while (index < queue.length) {
     const [curNode, curDepth] = queue[index++];
     graph[curNode].forEach((nextNode) => {
-      if (!visited[nextNode]) {
+      if (depth[nextNode] === -1) {
         depth[nextNode] = curDepth + 1;
-        parent[nextNode] = curNode;
-        visited[nextNode] = true;
+        parent[0][nextNode] = curNode;
         queue.push([nextNode, depth[nextNode]]);
       }
     });
   }
 
+  for (let i = 1; i <= LOG; i++) {
+    for (let j = 1; j <= N; j++) parent[i][j] = parent[i - 1][parent[i - 1][j]];
+  }
+
   const result = [];
-  nodes.forEach(([a, b]) => {
-    [a, b] = depth[a] < depth[b] ? [a, b] : [b, a];
-    while (depth[a] !== depth[b]) b = parent[b];
-    while (a !== b) [a, b] = [parent[a], parent[b]];
-    result.push(a);
-  });
+  nodes.forEach(([a, b]) => result.push(query(LOG, parent, depth, a, b)));
 
   return result.join("\n");
 };
