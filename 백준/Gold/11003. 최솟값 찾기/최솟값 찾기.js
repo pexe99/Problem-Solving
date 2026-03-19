f9704b96824e2bbc04ef75ac3eb4167b850ba421
이동령 <79556112+pexe99@users.noreply.github.com>
2026-03-19 18:52:06 +0900

    [Gold I] Title: 최솟값 찾기, Time: 768 ms, Memory: 162228 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.js
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.js
@@ -1,13 +1,14 @@
-const fs = require("fs");
-
 class IO {
   constructor() {
-    this.offset = 0;
     this.fs = require("fs");
-    this.inputBuffer = fs.readFileSync(
+
+    this.inputOffset = 0;
+    this.inputBuffer = this.fs.readFileSync(
       process.platform === "linux" ? 0 : "input.txt"
     );
-    this.outputBuffer = "";
+
+    this.outputOffset = 0;
+    this.outputBuffer = new Uint8Array(12 * 5000000);
   }
 
   readInteger() {
@@ -15,36 +16,80 @@ class IO {
     let isMinus = false;
 
     while (
-      this.offset < this.inputBuffer.length &&
-      this.inputBuffer[this.offset] <= 32
+      this.inputOffset < this.inputBuffer.length &&
+      this.inputBuffer[this.inputOffset] <= 32
     )
-      this.offset++;
-    if (this.inputBuffer.length <= this.offset) return null;
+      this.inputOffset++;
 
-    if (this.inputBuffer[this.offset] === 45) {
+    if (this.inputOffset >= this.inputBuffer.length) return null;
+
+    if (this.inputBuffer[this.inputOffset] === 45) {
       isMinus = true;
-      this.offset++;
+      this.inputOffset++;
     }
 
     while (
-      this.offset < this.inputBuffer.length &&
-      32 < this.inputBuffer[this.offset]
+      this.inputOffset < this.inputBuffer.length &&
+      this.inputBuffer[this.inputOffset] > 32
     )
-      result = result * 10 + (this.inputBuffer[this.offset++] - 48);
+      result = result * 10 + (this.inputBuffer[this.inputOffset++] - 48);
 
     return isMinus ? -result : result;
   }
 
-  addOutputBuffer(string) {
-    this.outputBuffer += string + " ";
-    if (this.outputBuffer.length % 10000 === 0) {
-      process.stdout.write(this.outputBuffer);
-      this.outputBuffer = "";
+  addOutputBuffer(num) {
+    let v = num;
+
+    if (v < 0) {
+      this.outputBuffer[this.outputOffset++] = 45;
+      v = -v;
     }
+
+    let r = v;
+
+    if (v >= 1000000000) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 1000000000) | 0);
+      r %= 1000000000;
+    }
+    if (v >= 100000000) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 100000000) | 0);
+      r %= 100000000;
+    }
+    if (v >= 10000000) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 10000000) | 0);
+      r %= 10000000;
+    }
+    if (v >= 1000000) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 1000000) | 0);
+      r %= 1000000;
+    }
+    if (v >= 100000) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 100000) | 0);
+      r %= 100000;
+    }
+    if (v >= 10000) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 10000) | 0);
+      r %= 10000;
+    }
+    if (v >= 1000) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 1000) | 0);
+      r %= 1000;
+    }
+    if (v >= 100) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 100) | 0);
+      r %= 100;
+    }
+    if (v >= 10) {
+      this.outputBuffer[this.outputOffset++] = 48 + ((r / 10) | 0);
+      r %= 10;
+    }
+
+    this.outputBuffer[this.outputOffset++] = 48 + r;
+    this.outputBuffer[this.outputOffset++] = 32;
   }
 
   writeOutputBuffer() {
-    process.stdout.write(this.outputBuffer.trimEnd());
+    this.fs.writeSync(1, this.outputBuffer.subarray(0, this.outputOffset - 1));
   }
 }
 
