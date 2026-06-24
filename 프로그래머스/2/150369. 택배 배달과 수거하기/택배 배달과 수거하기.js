270a2fd3d8a4b64f4aec65a249b2bd0d73385075
이동령 <79556112+pexe99@users.noreply.github.com>
2026-06-24 15:22:18 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 3.49 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,48 +1,19 @@
-/*
-[input]
-- cap: 트럭에 실을 수 있는 상자 최대 개수
-- n: 배달할 집의 개수
-- deliveries: 집마다 배달할 상자 개수
-- pickups: 집마다 수거할 상자 개수
-
-[output]
-- 모든 배달을 마치고 물류창고로 돌아오는 최소 이동 거리
-
-[solution]
-가장 먼 집부터 처리해 이동 거리를 최대한 줄이는 것이 포인트
-항상 cap만큼 싣고 출발해 가장 뒤에서부터 배달과 수거를 수행
-*/
-
 function solution(cap, n, deliveries, pickups) {
-    let result = 0;
+    let answer = 0;
+    let remainDeliveries = 0;
+    let remainPickups = 0;
     
-    while(deliveries.length || pickups.length) {
-        while(deliveries.at(-1) === 0) deliveries.pop();
-        while(pickups.at(-1) === 0) pickups.pop();
-        let curDist = Math.max(deliveries.length, pickups.length);
+    for(let i = n - 1; i >= 0; i--) {
+        remainDeliveries += deliveries[i];
+        remainPickups += pickups[i];
         
-        let curDelivery = cap;
-        while(curDelivery && deliveries.length) {
-            if(deliveries.at(-1) <= curDelivery) {
-                curDelivery -= deliveries.pop();
-            } else {
-                deliveries[deliveries.length - 1] -= curDelivery;
-                curDelivery = 0;
-            }
+        if(remainDeliveries > 0 || remainPickups > 0) {
+            const counter = Math.ceil(Math.max(remainDeliveries, remainPickups) / cap);
+            remainDeliveries -= cap * counter;
+            remainPickups -= cap * counter;
+            answer += (i + 1) * counter * 2;
         }
-        
-        let curPickup = cap;
-        while(curPickup && pickups.length) {
-            if(pickups.at(-1) <= curPickup) {
-                curPickup -= pickups.pop();
-            } else {
-                pickups[pickups.length - 1] -= curPickup;
-                curPickup = 0;
-            }
-        }
-        
-        result += curDist * 2;
     }
     
-    return result;
+    return answer;
 }
