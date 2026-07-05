44e434aaec58215d6d28959f815edd799da5abd1
이동령 <79556112+pexe99@users.noreply.github.com>
2026-07-05 15:45:46 +0900

    [level 1] Title: 노란불 신호등, Time: 14.98 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.js
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.js
@@ -1,14 +1,15 @@
 const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+
 const lcm = (a, b) => a * b / gcd(a, b);
 
-function solution(signals) {
-    const peroid = signals.map(([g, y, r]) => g + y + r);
-    const signalLCM = peroid.reduce((acc, cur) => lcm(acc, cur), 1);
+const solution = (signals) => {
+    const period = signals.map(([G, Y, R]) => G + Y + R);
+    const signalsLCM = period.reduce((acc, cur) => lcm(acc, cur), 1);
     
-    for(let i = 0; i < signalLCM; i++) {
-        const isYellow = signals.every((signal, idx) => {
-            const position = i % peroid[idx];
-            return signal[0] <= position && position < signal[0] + signal[1];
+    for(let i = 0; i < signalsLCM; i++) {
+        const isYellow = signals.every(([G, Y, R], idx) => {
+            const current = i % period[idx];
+            return G <= current && current < G + Y;
         });
         if(isYellow) return i + 1;
     }
