c5e702ba95efa5b473b94f715d0d3a4eb50031be
이동령 <79556112+pexe99@users.noreply.github.com>
2026-07-05 17:37:03 +0900

    [level 1] Title: 유연근무제, Time: 0.55 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.js
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -1,15 +1,14 @@
-function solution(schedules, timelogs, startday) {
-    schedules = schedules.map((schedule) => {
-        schedule += 10;
-        if(schedule % 100 >= 60) schedule += 40;
-        return schedule;
-    });
+const solution = (schedules, timelogs, startday) => {
+    startday--;
+    schedules = schedules.map((time) => 
+                60 <= (time + 10) % 100 ? time + 50 : time + 10);
+    
     return timelogs.filter((timelog, index) => {
-        let current = 0;
-        timelog.forEach((time, date) => {
-            date += startday - 1;
-            if(time <= schedules[index] && date % 7 < 5) current++;
-        });
-        return current === 5;
+        let counter = 0;
+        for(let i = 0; i < 7; i++) {
+            const curDate = (startday + i) % 7;
+            if(curDate < 5 && timelog[i] <= schedules[index]) counter++;
+        }
+        return counter === 5;
     }).length;
 }
