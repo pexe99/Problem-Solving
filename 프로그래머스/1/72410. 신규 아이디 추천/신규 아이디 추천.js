91b89892685af396ceb2eb32fb256ac5cc3a6d75
이동령 <79556112+pexe99@users.noreply.github.com>
2026-07-05 17:12:34 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.79 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,11 +1,12 @@
 function solution(new_id) {
     new_id = new_id
         .toLowerCase()
-        .replace(/[^0-9a-z._-]/g, "")
-        .replace(/[.]+/g, ".")
-        .replace(/^[.]|[.]$/g, "")
-        .replace(/^$/g, "a")
-        .slice(0, 15).replace(/^[.]|[.]$/g, "");
+        .replaceAll(/[^0-9a-z._-]/g, '')
+        .replaceAll(/[.]+/g, '.')
+        .replaceAll(/^[.]|[.]$/g, '')
+        .replaceAll(/^$/g, 'a')
+        .slice(0, 15)
+        .replaceAll(/^[.]|[.]$/g, '');
     
     return new_id.length <= 2 ? new_id.padEnd(3, new_id.at(-1)) : new_id;
 }
