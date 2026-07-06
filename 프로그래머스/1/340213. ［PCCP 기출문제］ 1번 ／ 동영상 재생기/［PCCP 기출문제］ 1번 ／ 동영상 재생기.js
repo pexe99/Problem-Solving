228f890430a9aba1a1334ee2f29a76fa4f13f850
이동령 <79556112+pexe99@users.noreply.github.com>
2026-07-06 18:42:37 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.17 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,29 +1,29 @@
-const OPERATIONS = { next: 10, prev: -10 };
-
-const getTimeNum = (timeStr) => {
-    const [min, sec] = timeStr.split(":");
+const getSeconds = (timeFormat) => {
+    const [min, sec] = timeFormat.split(':');
     return +min * 60 + +sec;
 }
 
-const getTimeStr = (timeNum) => {
-    const min = String(Math.floor(timeNum / 60)).padStart(2, '0');
-    const sec = String(timeNum % 60).padStart(2, '0');
+const getTimeFormat = (seconds) => {
+    const min = String(Math.floor(seconds / 60)).padStart(2, '0');
+    const sec = String(seconds % 60).padStart(2, '0');
     return `${min}:${sec}`;
 }
 
-function solution(video_len, pos, op_start, op_end, commands) {
-    video_len = getTimeNum(video_len);
-    pos = getTimeNum(pos);
-    op_start = getTimeNum(op_start);
-    op_end = getTimeNum(op_end);
+const solution = (video_len, pos, op_start, op_end, commands) => {
+    const operation = {'prev': -10, 'next': 10};
+    video_len = getSeconds(video_len);
+    pos = getSeconds(pos);
+    op_start = getSeconds(op_start);
+    op_end = getSeconds(op_end);
     
     if(op_start <= pos && pos <= op_end) pos = op_end;
+    
     commands.forEach((command) => {
-        pos += OPERATIONS[command];
+        pos = pos + operation[command];
         if(pos < 0) pos = 0;
-        else if(video_len < pos) pos = video_len;
+        if(video_len < pos) pos = video_len;
         if(op_start <= pos && pos <= op_end) pos = op_end;
     });
     
-    return getTimeStr(pos);
+    return getTimeFormat(pos);
 }
