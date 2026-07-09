7753064e7806ef2ba4224ef7dc7eb0256f198de1
이동령 <79556112+pexe99@users.noreply.github.com>
2026-07-09 16:31:17 +0900

    [level 1] Title: 공원 산책, Time: 0.93 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,23 +1,35 @@
-const op = {N : [-1, 0], S : [1, 0], W : [0, -1], E : [0, 1]};
+const op = {
+    'N': [-1, 0],
+    'S': [1, 0],
+    'W': [0, -1],
+    'E': [0, 1],
+};
 
-function solution(park, routes) {
+const solution = (park, routes) => {
     let dog = [0, 0];
-    park.forEach((row, index) => {
-        if(row.indexOf('S') !== -1) dog = [index, row.indexOf('S')];
-    });
-
-    for (let route of routes) {
-        const [way, far] = route.split(" ");
-        let [nx, ny] = [...dog], flag = true;
-        for (let i = 0; i < far; i++) {
+    const n = park.length;
+    const m = park[0].length;
+    
+    for(let x = 0; x < n; x++) {
+        const y = park[x].indexOf('S');
+        if(y !== -1) {
+            dog = [x, y];
+            break;
+        }
+    }
+    
+    routes.forEach((command) => {
+        const [way, far] = command.split(' ');
+        let [nx, ny] = [...dog], isAvailable = true;
+        for(let i = 0; i < +far; i++) {
             [nx, ny] = [nx + op[way][0], ny + op[way][1]];
-            if (!park[nx] || !park[nx][ny] || park[nx][ny] === 'X') {
-                flag = false;
+            if(!park[nx] || !park[nx][ny] || park[nx][ny] === 'X') {
+                isAvailable = false;
                 break;
             }
         }
-        if (flag) dog = [nx, ny];
-    }
-
+        if(isAvailable) dog = [nx, ny];
+    });
+    
     return dog;
 }
