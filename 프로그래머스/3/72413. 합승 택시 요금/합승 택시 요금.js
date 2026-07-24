5dc47fb48e276d45134919aa44c6e01c7c049529
이동령 <79556112+pexe99@users.noreply.github.com>
2026-07-24 16:33:31 +0900

    [level 3] Title: 합승 택시 요금, Time: 40.84 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,25 +1,38 @@
-function solution(n, s, a, b, fares) {
-    const dist = Array.from({length: n + 1}, (_, i) => {
-        return Array.from({length: n + 1}, (_, j) => {
-            return i === j ? 0 : Infinity;
-        });
-    });
-    
-    fares.forEach(([from, to, fare]) => {
-        dist[from][to] = dist[to][from] = fare;
-    });
-    
-    for(let k = 1; k <= n; k++) {
-        for(let i = 1; i <= n; i++) {
-            for(let j = 1; j <= n; j++) {
-                dist[i][j] = Math.min(dist[i][k] + dist[k][j], dist[i][j]);
+const dijkstra = (start, n, graph) => {
+    const dist = new Array(n + 1).fill(Infinity);
+
+    dist[start] = 0;
+    const queue = [[start, 0]];
+        
+    while(queue.length) {
+        const [curNode, curDist] = queue.shift();
+        if(dist[curNode] < curDist) continue;
+        Object.entries(graph[curNode]).forEach(([nextNode, nextDist]) => {
+            const distSum  = curDist + nextDist;
+            if(distSum < dist[nextNode]) {
+                dist[nextNode] = distSum;
+                queue.push([nextNode, distSum]);
             }
-        }
+        });
+        queue.sort((a, b) => a[1] - b[1]);
     }
+    
+    return dist;
+}
+
+const solution = (n, s, a, b, fares) => {
+    const graph = Array.from({length: n + 1}, () => ({}));
+    fares.forEach(([from, to, fare]) => {
+        graph[from][to] = graph[to][from] = fare;
+    });
+    
+    const fromS = dijkstra(s, n, graph);
+    const fromA = dijkstra(a, n, graph);
+    const fromB = dijkstra(b, n, graph);
     
     let result = Infinity;
     for(let i = 1; i <= n; i++) {
-        result = Math.min(result, dist[s][i] + dist[i][a] + dist[i][b]);
+        result = Math.min(result, fromS[i] + fromA[i] + fromB[i]);
     }
     
     return result;
