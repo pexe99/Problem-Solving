5b77a519a263f7915ddeb99292ddb2d560b39947
이동령 <79556112+pexe99@users.noreply.github.com>
2026-08-17 17:06:13 +0900

    [level 4] Title: 사칙연산, Time: 0.36 ms, Memory: 44.3 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.js
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.js
@@ -1,33 +1,19 @@
 const solution = (arr) => {
-    const number = [];
-    const operation = [];
-    arr.forEach((e, i) => i % 2 ? operation.push(e) : number.push(+e));
-    
-    const n = number.length;
-    const dp = Array.from({length: n}, (_, i) => {
-        return Array.from({length: n}, (_, j) => {
-            const current = {max: -Infinity, min: Infinity};
-            if(i === j) current.max = current.min = number[i];
-            return current;
-        });
-    });
-    
-    for(let i = n - 2; i >= 0; i--) {
-        for(let j = i + 1; j < n; j++) {
-            for(let k = i; k < j; k++) {
-                let curMax, curMin;
-                if(operation[k] === '+') {
-                    curMax = dp[i][k].max + dp[k + 1][j].max;
-                    curMin = dp[i][k].min + dp[k + 1][j].min;
-                } else {
-                    curMax = dp[i][k].max - dp[k + 1][j].min;
-                    curMin = dp[i][k].min - dp[k + 1][j].max;
-                }
-                dp[i][j].max = Math.max(dp[i][j].max, curMax);
-                dp[i][j].min = Math.min(dp[i][j].min, curMin);
-            }
+    const tails = arr.join('')
+                     .split('-')
+                     .map((e) => e.split('+').map((e) => +e))
+                     .reverse();
+
+    return tails.reduce(([maxTail, minTail], sub, idx) => {
+        const minSum = sub.reduce((acc, cur) => acc - cur, 0);
+        const maxSum = -minSum - 2 * sub[0];
+        
+        if(idx === tails.length - 1) {
+            return -minSum + maxTail;
+        } else {
+            let curMax = Math.max(maxSum + maxTail, minSum - minTail);
+            let curMin = Math.min(minSum + minTail, minSum - maxTail);
+            return [curMax, curMin];
         }
-    }
-    
-    return dp[0][n - 1].max;
+    }, [0, 0]);
 }
